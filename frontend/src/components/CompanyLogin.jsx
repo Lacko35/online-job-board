b5294d7b5c7ecfabe09aa603ad0a243f41473a34
lazar <lazar@gmail.com>
2026-09-06 15:03:30 +0200

    adding style for login cards

--- frontend/src/components/CompanyLogin.jsx
+++ frontend/src/components/CompanyLogin.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
+
 export default function CompanyLogin() {
+    const [companyObj, setCompanyObj] = useState({
+        pib: '',
+        password: ''
+    });
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        
+        setCompanyObj(prev => ({
+            [name]: value
+        }));
+    }
+
     return (
         <div className="company-login-container">
-            
+            <label htmlFor="pib-field">
+                Enter PIB:
+
+                <input 
+                    type="text" 
+                    name="pib"
+                    id="pib-field"
+                    value={companyObj.pib}
+                    onChange={e => handleChange(e)}
+                />
+            </label>
+
+            <label htmlFor="password-field">
+                Enter password:
+
+                <input 
+                    type="password" 
+                    name="password"
+                    id="password-field"
+                    value={companyObj.password}
+                    onChange={e => handleChange(e)}
+                />
+            </label>
+
+            <div>
+                <button>LOGIN</button>
+            </div>
         </div>
     )
 }
--- frontend/src/components/WorkerLogin.jsx
+++ frontend/src/components/WorkerLogin.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react"
+
 export default function WorkerLogin() {
+    const [workerObj, setWorkerObj] = useState({
+        username: '',
+        password: ''
+    });
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        
+        setCompanyObj(prev => ({
+            [name]: value
+        }));
+    }
+
     return (
-        <div className="worker-login-container"></div>
+        <div className="worker-login-container">
+            <label htmlFor="username-field">
+                Enter your username:
+
+                <input 
+                    type="text" 
+                    name="username"
+                    id="username-field"
+                    value={workerObj.username}
+                    onChange={e => handleChange(e)}
+                />
+            </label>
+
+            <label htmlFor="password-field">
+                Enter password:
+
+                <input 
+                    type="password" 
+                    name="password"
+                    id="password-field"
+                    value={workerObj.password}
+                    onChange={e => handleChange(e)}
+                />
+            </label>
+
+            <div>
+                <button>LOGIN</button>
+            </div>
+        </div>
     )
 }
